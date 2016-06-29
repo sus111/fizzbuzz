f39d0ece0b3fd3786654bad619d4c609c15c8f3c
sus111 <sgraham535@gmail.com>
2016-06-29 10:54:25 +0100

    finishing fizzbuzz.rb

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,36 @@
+require 'fizzbuzz'
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns 4 when passed 4' do
+    expect(fizzbuzz(4)).to eq 4
+  end
+
+  it 'returns "fizz" when passed 6' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
+
+  it 'returns "buzz" when passed 10' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
+
+  it 'returns 7 when passed 7' do
+    expect(fizzbuzz(7)).to eq 7
+  end
+
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+
+  it 'returns "WARNING! ERROR!" when passed "Foosball"' do
+    expect(fizzbuzz("Foosball")).to eq "WARNING! ERROR!"
+  end
+
+end
